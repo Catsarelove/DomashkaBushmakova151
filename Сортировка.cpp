c31cc133ad73a0c42af9ce7e4598272d5eccec85
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-09-27 22:38:25 +0500

    Add files via upload

---
+++ Сортировка.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int*a;
+	int n;
+	cin>>n;
+	a=new int[n];
+	for(int i=0; i<n; i++)
+		cin>>(*(a+i));
+	for(int i=1; i<n; i++)
+	{  if(*(a+i-1)>*(a+i))
+		{ int c=*(a+i);
+		  int right=i-1, left=0;
+		  do
+		 {
+		    int mid=(right+left)/2;
+			
+			if(*(a+mid)<c) left=mid+1;
+			else 
+				right=mid-1;
+		 }
+		 while(left<=right);
+		 
+		 
+		 for(int j=i-1; j>=left; j--)
+			 *(a+j+1)=*(a+j);
+		     *(a+left)=c;
+	   }
+	}
+	for(int i=0; i<n; i++)
+		cout<<*(a+i);
+	system("pause");
+	return 0;
+}
