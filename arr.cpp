2dba904f0d6c1447209b38757cdcdfaea71426e4
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-10-04 22:11:13 +0300

    Add files via upload
    Old domashka for the previous lesson

---
+++ arr.cpp
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int*a;
+    a=new int[n*m];
+	for(int i=0, k=0; i<n; i++, k+=m-1)
+		for(int j=0; j<m; j++)
+          cin>>(*(a+i+j+k));
+	for(int i=0, k=0; i<n; i++, k+=m-1)
+		{
+				for(int j=0; j<m; j++)
+				cout<<(*(a+i+j+k))<<' ';
+				cout<<'\n';
+		}
+	
+	delete [n*m] a;
+	system("pause");
+	return 0;
+}
