484279d2f08b35a7adf77057059462ecfe9abbbc
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-12-11 22:14:32 +0300

    Update Task1(Rewriting).cpp
    Accidentally downloaded homework code instead of the one from the rewriting

--- Contr2/Task1(Rewriting).cpp
+++ Contr2/Task1(Rewriting).cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 using namespace std;
-int cubeee(int x) {
-	if (x < 5) {
+int cubeee(int x, int & mini) {
+	if (x < 8) {
+		mini = 1;
 		return x;
 	}
-	double a = (double)pow(x - 1, (1. / 2));
+	double a = (double)pow(x, (1. / 3));
 	if (a - (int)a == 0) {
+		mini = x;
 		return 1;
 	}
+	int temp;
 	int min = x;
-	for (int i = 1; i*i + 1 < x; i++) {
-		int c = x / (i*i + 1) + cubeee(x % (i*i + 1));
-		if (c <= min)
+	mini = 1;
+	for (int i = 1; i*i*i < x; i++) {
+		int c = x / (i*i*i) + cubeee(x % (i*i*i), temp);
+		if (c <= min) {
 			min = c;
-	}
-	return min;
+			mini = i*i*i;
+		}
+    }
+		return min;
+}
+void napisau(int x) {
+	int mini = x;
+	int t = cubeee(x, mini);
+	for(int i=0; i<x/mini; i++)
+	cout << " + " << mini ;
+	if (x % mini != 0)
+		napisau(x % mini);
+
 }
 int main()
 {
 	int x, t, mini;
 	cin >> x;
 	mini = x;
-	t = cubeee(x);
+	t = cubeee(x, mini);
 	cout << "quantity = " << t << endl;
+	for (int i = 0; i < x / mini; i++) {
+		if (i > 0) cout << " + ";
+		cout << mini;
+	}
+	napisau(x % mini);
+	cout << endl;
 	system("pause");
 	return 0;
 }
