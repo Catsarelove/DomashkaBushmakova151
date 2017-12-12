3225056f3329dbea08de7c11caaea56b1198aed2
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-12-12 20:02:18 +0500

    Add files via upload

--- September/Contr1/Task2.cpp
+++ September/Contr1/Task2.cpp
@@ -1,39 +1,50 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-struct complex{
-	float Im;
-	float Re;
-};
-complex sum(complex a, complex b)
-{ 
-	complex c;
-	c.Im=a.Im+b.Im;
-	c.Re=a.Re+b.Re;
-	return c;
+void wind(int**a, int m,int n, int t){
+	int i=n-1;
+	int o;
+	int j=m-1;
+	for(o=j-1; o>=t; o--)
+		a[i][o]=a[i][o+1]+1;
+	j=t;
+	for(o=i-1; o>=t; o--)
+		  a[o][j]=a[o+1][j]+1;
+	i=t;	
+	for(o=j+1; o<m; o++)
+		a[i][o]=a[i][o-1]+1;
+     j=m-1;
+	for(o=i+1; o<n-1; o++)
+		a[o][j]=a[o-1][j]+1;
+     i=n-1;
 }
-complex mul(complex a, complex b)
-{
-	complex c;
-	c.Im=a.Re*b.Im+b.Re*a.Im;
-	c.Re=a.Re*b.Re-a.Im*b.Im;
-	return c;
-}
-complex abssq(complex a)
-{
-	complex c;
-	c.Im=2*a.Im*a.Re;
-	c.Re=a.Re*a.Re-a.Im*a.Im;
-	return c;
-}
+
 int main()
 {
-	complex x,y;
-	cin>>x.Re>>x.Im>>y.Re>>y.Im;
-	cout<<"x = "<<x.Re<<showpos<<x.Im<<noshowpos<<"i"<<endl;
-	cout<<"y = "<<y.Re<<showpos<<y.Im<<noshowpos<<"i"<<endl;
-	cout<<"x + y = "<<(sum(x,y).Re)<<showpos<<(sum(x,y).Im)<<noshowpos<<"i"<<endl;
-	cout<<"x * y = "<<(mul(x,y).Re)<<showpos<<(mul(x,y).Im)<<noshowpos<<"i"<<endl;
-	cout<<"x^2 = "<<(abssq(x).Re)<<showpos<<(abssq(x).Im)<<noshowpos<<"i"<<endl<<"y^2 = "<<(abssq(y).Re)<<showpos<<(abssq(y).Im)<<noshowpos<<"i"<<endl;
-	system("pause");
+	int**a;
+	int n, m;
+	cin >> n >> m;
+	a=new int*[n];
+	for (int i = 0; i < n; i++)
+		a[i] = new int[m];
+	a[n-1][m-1]=1;
+	int tn=n; int tm=m;
+	int min=tm>tn?tn:tm;
+	int t=0;
+	while(tn!=(min+1)/2 && tm!=(min+1)/2)
+	{
+		wind(a, tm, tn,t);
+		if(tn!=(min+2)/2 && tm!=(min+2)/2)
+		a[tn-2][tm-2]=a[tn-2][tm-1]+1;
+		tn--; tm--;
+		t++;
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++)
+			cout<<a[i][j]<<'\t';
+		cout << endl;
+	}
+	system ("pause");
+	delete [] a;
 	return 0;
 }
