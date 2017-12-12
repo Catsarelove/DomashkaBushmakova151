f5b63bd60fdb1166680303c478348fd2d4df9854
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-12-12 23:26:48 +0500

    Add files via upload

---
+++ HomeTask17.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+struct node {
+	int data;
+	node* next;
+};
+struct dequeue {
+	node* tail;
+	node* head;
+	void pushback(int new_data) {
+		node * temp = new node;
+		temp->data = new_data;
+		node * paste = tail;
+		if (tail != NULL) {
+			tail = temp;
+			paste->next = tail;
+		}
+		else {
+			tail = temp;
+			head = temp;
+		}
+		temp->next = NULL;
+    }
+	void pushforw(int new_data) {
+		node * temp = new node;
+		temp->data = new_data;
+		node * paste = head;
+		if (head != NULL) {
+			head = temp;
+			head->next = paste;
+		}
+		else {
+			head = temp;
+			tail = temp;
+			head->next = NULL;
+		}
+	}
+	int pop() {
+		if (head == NULL) {
+			cout << "OMG";
+			return 0;
+		}
+		int temp_data = head->data;
+		node* temp = head;
+		head = head->next;
+		delete[] temp;
+		return temp_data;
+	}
+	~dequeue() {
+		while (head!= NULL) {
+			node* temp = head;
+			head = head->next;
+			delete temp;
+		}
+	}
+};
+int main() {
+
+	dequeue kuoe;
+	kuoe.tail = NULL;
+	kuoe.head = NULL;
+	kuoe.pushforw(33);
+	kuoe.pushback(2);
+	kuoe.pushforw(8);
+	cout << kuoe.pop() << endl;
+	cout << kuoe.pop() << endl;
+	cout << kuoe.pop() << endl;
+	kuoe.~dequeue();
+	system("pause");
+	return 0;
+}
