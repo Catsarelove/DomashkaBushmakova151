acc010318d9db0e1a5b4faff6e50c98c8775ad11
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-12-17 12:42:00 +0300

    Update HomeTask8.cpp

--- HomeTask8.cpp
+++ HomeTask8.cpp
@@ -1,6 +1,7 @@
+
 #include <iostream>
 using namespace std;
-void Quick(int* a, int L, int R)
+void Quick(int* a, int L, int R, int k)
 {
 	int i, j, bar;
 	i = L;
@@ -8,9 +9,9 @@ void Quick(int* a, int L, int R)
 	bar = a[(L + R) / 2];
 	do
 	{
-		while (a[i]<bar)
+		while (a[i]>bar)
 			i++;
-		while (a[j]>bar)
+		while (a[j]<bar)
 			j--;
 		if (i <= j)
 		{
@@ -18,8 +19,8 @@ void Quick(int* a, int L, int R)
 			i++; j--;
 		}
 	} while (i <= j);
-	if (L<j) Quick(a, L, j);
-	if (R>i) Quick(a, i, R);
+	if (L<j && k >= L && k <=j) Quick(a, L, j, k);
+	if (R>i && k <=R && k>=i ) Quick(a, i, R, k);
 
 }
 int main()
@@ -28,19 +29,12 @@ int main()
 	int n, k;
 	cin >> n >> k;
 	if (n <= k) { cout << "not found" << endl; return 0; }
+	k = k - 1;
 	a = new int[n];
 	for (int i = 0; i<n; i++)
 		cin >> a[i];
-	Quick(a, 0, n - 1);
-	int j = n - k;
-	if (k != 1) {
-		while (j != 0 && a[j] == a[j + 1])
-			j--;
-		if (a[j] == a[j + 1])
-			cout << "not found" << endl;
-		else cout << a[j] << endl;
-	}
-	else cout << a[j] << endl;
+	Quick(a, 0, n - 1, k);
+        cout << a[k] << endl;
 	system("pause");
 	delete[] a;
 	return 0;
