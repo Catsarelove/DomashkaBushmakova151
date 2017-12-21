0ed86b45092c3c771e1bd6112724fd924ff9738d
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2017-12-21 18:20:01 +0300

    Update HomeTask9.cpp

--- HomeTask9.cpp
+++ HomeTask9.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 using namespace std;
 int x, y;
-int qcells(int ** a, int i, int j)
+int qcells(int ** a, int ** way, int x, int y, int x1, int y1)
 {
-	if(i == x && j == y) return a[i][j];
-	if(a[i][j] == 0) return 0;
-    int s = 0;
-	if(i >= 1) s += qcells(a, i - 1, j);
-	if(j >= 1) s += qcells(a, i, j - 1);
-	if((i >= 1) && (j >= 1)) s += qcells(a, i - 1, j - 1);
-	if(i >= 2) s += qcells(a, i - 2, j);
-	if(j >= 2) s += qcells(a, i, j - 2);
-	if((i >= 2) && (j >= 2)) s += qcells(a, i - 2, j - 2);
-	return s;
+	if (a[x][y]==0) return 0;
+		way[x][y] = 1;
+		for (int i = x; i <= x1; i++) 
+			for (int j = y; j <= y1; j++) {
+				if (a[i][j]!=0) {
+					if (i > 0) way[i][j] += way[i - 1][j];
+					if (j > 0) way[i][j] += way[i][j - 1];
+					if (j > 0 && i > 0) way[i][j] += way[i - 1][j - 1];
+					if (i > 1) way[i][j] += way[i - 2][j];
+					if (j > 1) way[i][j] += way[i][j - 2];
+					if (j >	1 && i > 1) way[i][j] += way[i - 2][j - 2];
+				}
+			}
+		return way[x1][y1];
 }
 int main()
 {
 	int n, m, x1, y1; // A(x, y) >>> B(x1, y1)
-	cin>>n>>m;
+	cin >> n >> m;
 	int **a;
-	a = new int*[n];
-	for(int i=0; i<n; i++)
-		a[i]=new int[m];
-	for(int i=0; i<n;i++)
-		for(int j=0; j<m; j++)
-			cin>>a[i][j];
-	cin>>x>>y>>x1>>y1;
-	cout<<qcells(a, x1, y1)<<endl;
+	int ** way;
+	way = new int*[n];
+    a = new int*[n];
+	for (int i = 0; i < n; i++) {
+		a[i] = new int[m];
+		way[i] = new int[m];
+		for (int j = 0; j < m; j++)
+			way[i][j] = 0;
+	}
+	for (int i = 0; i<n;i++)
+		for (int j = 0; j<m; j++)
+			cin >> a[i][j];
+	cin >> x >> y >> x1 >> y1;
+	cout << qcells(a, way, x, y, x1, y1);
 	system("pause");
 	return 0;
 }
