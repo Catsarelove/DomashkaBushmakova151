0ade3d9d74c0ec24f692be56870adab06ef0af74
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2018-12-19 14:23:00 +0300

    Update HW_5.cpp

--- Term3/Task5/HW_5.cpp
+++ Term3/Task5/HW_5.cpp
@@ -44,3 +44,91 @@ int main() {
 	printf("%d", b);
 	return 0;
 }         
+#include <mutex>
+#include<thread>
+#include <algorithm>
+#include <stdio.h>
+#include <chrono>
+#include <vector>
+
+using namespace std;
+class LockedErr{};
+
+template <typename T>
+class A {
+	std::mutex m;
+	T data;
+public:
+	A(){}
+	A operator =(const T & x){
+		m.lock();
+		data = x;
+		m.unlock();	
+		return *this;
+	}
+	A operator =(const A & x){
+		m.lock();
+		data = x.data;
+		m.unlock();	
+		return *this;
+	}
+	T load(){
+		m.lock();
+		T x = data;
+		m.unlock();
+		return x; 
+	} 
+	void store( const T & x){
+		m.lock();
+		data = x;
+		m.unlock(); 
+	}
+	operator T() const{
+		return data;
+	}
+private:
+	A(const A & a){}
+	A(const A && a){}
+};
+void foo(int * b){
+	sort(b, b+n);
+}
+
+void Sort(A* array, int n)
+{
+  if(n > 1){
+    int size = n;
+    for(int i = 1; i < size; ++i){
+      int j = i - 1;
+      A key = array[i];
+      while(j >= 0 && array[j] > key){
+        array[j+1] = array[j];
+        --j;
+      }
+      array[j+1] = key;
+    }
+  }
+}
+
+
+int main()
+{
+    A* a;
+    int n = 12000;
+    a = new A[n];
+    for(int i = 0; i < n; i++)
+        a[i] = rand()%100 - 100;
+    thread t1(Sort, a, n);
+    thread t2(Sort, a, n);
+    t1.join();
+    t2.join();
+    bool check = true;
+    for(int i = 0; i< (n - 1) && check; i++){
+        if(a[i]>a[i+1]){
+            cout << "AAAAA";
+            check = false;
+        }
+    }
+    delete [] a;
+    return 0;
+}         
